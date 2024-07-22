ce55b9f8bd47939e1b13ffaa14c55f6b8faf2b27
Perry102 <141802227+Perry102@users.noreply.github.com>
2024-07-23 00:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 //your code here
+
+
+.container {
+    margin-bottom: 20px;
+}
+
+.block-inline-div,
+.block-inline-p,
+.block-inline-h1 {
+    margin-bottom: 10px;
+}
+
+/* Inline elements */
+.block-inline-button,
+.block-inline-span,
+.block-inline-input,
+.block-inline-cite,
+.block-inline-code,
+.block-inline-sup,
+.block-inline-sub {
+    display: inline-block; 
+    margin-right: 10px; 
+}
+
+.block-inline-div,
+.block-inline-p,
+.block-inline-h1 {
+    display: block;
+    margin-bottom: 10px; 
+}
+
+.block-inline-div,
+.block-inline-p,
+.block-inline-h1 {
+    background-color: #f0f0f0;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+.block-inline-button,
+.block-inline-span,
+.block-inline-input,
+.block-inline-cite,
+.block-inline-code,
+.block-inline-sup,
+.block-inline-sub {
+    background-color: #eaf2ff;
+    padding: 5px 10px;
+    border: 1px solid #3498db;
+    border-radius: 3px;
+}
